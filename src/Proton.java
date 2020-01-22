cdcbdcf9ac64722c1c13e5885efe8ba3f707a89b
David Hein <david-hein196@web.de>
2020-01-22 08:49:57 +0100

    Proton does not know proton trap

--- src/Proton.java
+++ src/Proton.java
@@ -2,8 +2,6 @@ public class Proton {
     private int[][][] structure;
     private double weight;
 
-    private ProtonTrap protonTrap;
-
     public Proton() {
         this. structure = new int[100][100][100];
     }
