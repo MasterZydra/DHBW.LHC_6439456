e19560ac9d2c808ae0e5928ac0398b9d18b52993
David Hein <david-hein196@web.de>
2020-01-29 10:08:41 +0100

    Start implementation

--- src/infrastructure/lhc/Ring.java
+++ src/infrastructure/lhc/Ring.java
@@ -1,18 +1,23 @@
 package infrastructure.lhc;
 
+import com.google.common.eventbus.Subscribe;
 import infrastructure.LargeHadronCollider;
+import infrastructure.lhc.detector.Detector;
+import infrastructure.lhc.experiment.*;
 
-public class Ring implements IRing {
+public class Ring extends Subscriber implements IRing {
     private boolean isActivated;
     private Experiment currentExperiment;
     private int energy;
 
     private Magnet[] magnets;
-    private ProtonTrap protonTrap;
+    private ProtonTrap[] protonTrap;
     private Detector detector;
     private LargeHadronCollider lhc;
 
     public Ring() {
+        super();
+        this.protonTrap = new ProtonTrap[2];
         this.magnets = new Magnet[72];
         for (int i = 0; i < 72; i++) {
             this.magnets[i] = new Magnet();
@@ -48,6 +53,23 @@ public class Ring implements IRing {
     }
 
     public void shutdown() {
+
+    }
+
+    @Subscribe
+    public void receive(RunExperimentFullEvent event) {
+        activate(event.getInitialEnergy());
+        activateMagneticField();
+        releaseProton();
+        releaseProton();
+        while(this.energy < 300000) {
+            increaseEnergy(25000);
+        }
+        //collide();
+    }
+
+    @Subscribe
+    public void receive(RunExperimentPartialEvent event) {
 
     }
 }
