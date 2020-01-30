e7fe1cd414a42e5cb3cffbd169af0082886f585e
David Hein <david-hein196@web.de>
2020-01-30 15:28:23 +0100

    Add ID and getter for structure and ID

--- src/infrastructure/lhc/Proton.java
+++ src/infrastructure/lhc/Proton.java
@@ -1,10 +1,20 @@
 package infrastructure.lhc;
 
 public class Proton {
+    private int id;
     private int[][][] structure;
     private double weight;
 
-    public Proton(int[][][] structure) {
+    public Proton(int id, int[][][] structure) {
         this.structure = structure;
+        this.id = id;
+    }
+
+    public int[][][] getStructure() {
+        return this.structure;
+    }
+
+    public int getID() {
+        return this.id;
     }
 }
