05f3519cf9c0bbf4326444325044464f6da7ee94
David Hein <david-hein196@web.de>
2020-01-30 15:27:05 +0100

    Add setter for proton IDs Format time stamp Extend return value of toString()

--- src/infrastructure/lhc/experiment/Experiment.java
+++ src/infrastructure/lhc/experiment/Experiment.java
@@ -1,5 +1,7 @@
 package infrastructure.lhc.experiment;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
@@ -8,22 +10,30 @@ public class Experiment implements IExperiment {
     private String dateTimeStamp;
     private boolean isHiggsBosonFound;
 
+    private int[] protonIDs;
+
     private IBlock[] blocks;
 
     public Experiment() {
         this.uuid = UUID.randomUUID();
+        this.protonIDs = new int[2];
+
         this.blocks = new Block[200000];
         for (int i = 0; i < 200000; i++) {
             this.blocks[i] = new Block();
         }
 
-        this.dateTimeStamp = (new Date()).toString();
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
+        this.dateTimeStamp = (dateFormat.format(new Date()));
     }
 
     @Override
     public String toString() {
-        String ret = this.uuid + " - " + this.dateTimeStamp + " - ";
-        ret += this.isHiggsBosonFound ? "Higgs found" : "Higgs not found";
+        String ret = this.isHiggsBosonFound ? "Higgs found" : "NO Higgs found";
+        ret += " -- ";
+        ret += this.uuid + " - " + this.dateTimeStamp;
+        ret += " - Protons: " + Integer.toString(this.protonIDs[0]);
+        ret += ", " + Integer.toString(this.protonIDs[1]);
         return ret;
     }
 
@@ -33,5 +43,10 @@ public class Experiment implements IExperiment {
 
     public void setHiggsBosonFound() {
         this.isHiggsBosonFound = true;
+    }
+
+    public void setProtonIDs(int protonId1, int protonId2) {
+        this.protonIDs[0] = protonId1;
+        this.protonIDs[1] = protonId2;
     }
 }
