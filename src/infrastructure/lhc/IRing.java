990ad8c0f07e2d33e63f59aa265b11471853f26b
David Hein <david-hein196@web.de>
2020-01-30 15:37:36 +0100

    Add setter for proton traps and detector Remove parameter for collide() Implement against interface Implement logic

--- src/infrastructure/lhc/IRing.java
+++ src/infrastructure/lhc/IRing.java
@@ -1,12 +1,16 @@
 package infrastructure.lhc;
 
+import infrastructure.lhc.detector.IDetector;
+
 public interface IRing {
     void activate();
     void activate(int initialEnergy);
     void activateMagneticField();
     void releaseProton();
     void increaseEnergy(int delta);
-    void collide(Proton proton01, Proton proton02);
+    void collide();
     int decreaseEnergy();
     void shutdown();
+    void setProtonTraps(ProtonTrap protonTrap1, ProtonTrap protonTrap2);
+    void setDetector(IDetector detector);
 }
