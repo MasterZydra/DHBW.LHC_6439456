7cc6e3c113812ee305a4eb0cc11cc9ad8ded49d5
David Hein <david-hein196@web.de>
2020-01-30 15:25:55 +0100

    Add setter Set uuid in constructor

--- src/infrastructure/lhc/experiment/Block.java
+++ src/infrastructure/lhc/experiment/Block.java
@@ -8,7 +8,15 @@ public class Block implements IBlock {
     private UUID uuid;
     private String structure;
 
+    public Block() {
+        this.uuid = UUID.randomUUID();
+    }
+
     public String getStructure() {
         return this.structure;
+    }
+
+    public void setStructure(String structure) {
+        this.structure = structure;
     }
 }
--- src/infrastructure/lhc/experiment/IBlock.java
+++ src/infrastructure/lhc/experiment/IBlock.java
@@ -2,4 +2,5 @@ package infrastructure.lhc.experiment;
 
 public interface IBlock {
     String getStructure();
+    void setStructure(String structure);
 }
