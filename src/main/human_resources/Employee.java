74389dccd0f08d82aa2bf82ed175567ed8fefa5b
David Hein <david-hein196@web.de>
2020-02-13 19:21:33 +0100

    Add function to get data for sql insert statement

--- src/main/human_resources/Employee.java
+++ src/main/human_resources/Employee.java
@@ -8,4 +8,17 @@ public abstract class Employee extends Person {
     public Employee(int id, String name) {
         super(id, name);
     }
+
+    public String getDataBaseString() {
+        StringBuilder ret = new StringBuilder(Integer.toString(this.id) + ", '" + this.name + "', '");
+        for (int i = 0; i < this.iris.length; i++) {
+            for (int j = 0; j < this.iris[i].length; j++) {
+                ret.append(Integer.toString(this.iris[i][j])).append(",");
+            }
+        }
+        ret.append("', '").append(this.getIdCard().getId()).append("', '");
+        ret.append(this.getFingerPrint()).append("'");
+
+        return ret.toString();
+    }
 }
