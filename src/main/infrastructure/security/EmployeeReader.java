08d119b60a19ec79f8b3bfe26f065a603c943a04
David Hein <david-hein196@web.de>
2020-02-13 08:55:25 +0100

    Remove property and use only property from Reader

--- src/main/infrastructure/security/EmployeeReader.java
+++ src/main/infrastructure/security/EmployeeReader.java
@@ -3,25 +3,21 @@ package main.infrastructure.security;
 import java.util.Date;
 
 public class EmployeeReader extends Reader {
-    private EmployeeIDCard currentEmployeeIDCard; // TODO: löschen
-
     public void insertIDCard(EmployeeIDCard idCard) {
         if (idCard.getIsLocked() || idCard.getValidUntil().compareTo(new Date()) < 0)
         {
             System.out.println("IDCard was denied");
             return;
         }
-        this.currentEmployeeIDCard = idCard;
         this.currentIDCard = idCard;
     }
 
     public boolean verifyPassword(String input) {
-        return this.checkPassword(input, this.currentEmployeeIDCard.getCommunication().getData());
+        return this.checkPassword(input, this.currentIDCard.getCommunication().getData());
     }
 
     @Override
     public void removeIDCard() {
-        this.currentEmployeeIDCard = null;
         this.currentIDCard = null;
     }
 }
--- src/main/infrastructure/security/VisitorReader.java
+++ src/main/infrastructure/security/VisitorReader.java
@@ -3,8 +3,6 @@ package main.infrastructure.security;
 import java.util.Date;
 
 public class VisitorReader extends Reader {
-    private VisitorIDCard currentVisitorIDCard;
-
     public void insertIDCard(VisitorIDCard idCard) {
         if (idCard.getIsLocked() || idCard.getValidUntil().compareTo(new Date()) < 0)
         {
@@ -12,16 +10,14 @@ public class VisitorReader extends Reader {
             return;
         }
         this.currentIDCard = idCard;
-        this.currentVisitorIDCard = idCard;
     }
 
     public boolean verifyPassword(String input) {
-        return this.checkPassword(input, this.currentVisitorIDCard.getCommunication().getData());
+        return this.checkPassword(input, this.currentIDCard.getCommunication().getData());
     }
 
     @Override
     public void removeIDCard() {
-        this.currentVisitorIDCard = null;
         this.currentIDCard = null;
     }
 }
