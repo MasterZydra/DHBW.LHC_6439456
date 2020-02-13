a232a12d75d5733ad1e7133796f4510133483e41
David Hein <david-hein196@web.de>
2020-02-13 19:14:26 +0100

    Override string to get data for insert statement

--- src/main/infrastructure/security/EmployeeIDCard.java
+++ src/main/infrastructure/security/EmployeeIDCard.java
@@ -14,4 +14,24 @@ public class EmployeeIDCard extends IDCard {
         this.passwordChip.setData(password);
         this.communication.setData(password);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder("'" + this.getId() + "', '");
+        ret.append(this.getValidFrom().toString() + "', '");
+        ret.append(this.getValidUntil().toString() + "', '");
+        for (int i = 0; i < this.getIrisStructure().length; i++) {
+            for (int j = 0; j < this.getIrisStructure()[i].length; j++) {
+                ret.append(Integer.toString(this.getIrisStructure()[i][j]) + ",");
+            }
+        }
+        ret.append("', " + this.getIsLocked() + ", ");
+        this.getPermissionList().forEach((e) -> {
+            ret.append(e.toString() + ',');
+        });
+        ret.append("', " + Integer.toString(this.getInvalidPasswordCounter()) + ", " + this.getPerson().getID());
+        ret.append(", '" + this.passwordChip + "', '" + this.fingerPrintChip + "'");
+
+        return ret.toString();
+    }
 }
