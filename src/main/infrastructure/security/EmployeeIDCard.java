66f2a9aa277d80aa7351482c666dc8e6a7bbb819
David Hein <david-hein196@web.de>
2020-02-17 09:32:27 +0100

    Format date for DB insert. Add missing quote

--- src/main/infrastructure/security/EmployeeIDCard.java
+++ src/main/infrastructure/security/EmployeeIDCard.java
@@ -1,5 +1,8 @@
 package main.infrastructure.security;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class EmployeeIDCard extends IDCard {
     private IChip fingerPrintChip;
     private IChip passwordChip;
@@ -17,15 +20,17 @@ public class EmployeeIDCard extends IDCard {
 
     @Override
     public String toString() {
-        StringBuilder ret = new StringBuilder("'" + this.getId() + "', '");
-        ret.append(this.getValidFrom().toString() + "', '");
-        ret.append(this.getValidUntil().toString() + "', '");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        StringBuilder ret = new StringBuilder("'" + this.getId() + "', DATE '");
+        ret.append(dateFormat.format(this.getValidFrom()) + "', DATE '");
+        ret.append(dateFormat.format(this.getValidUntil()) + "', '");
         for (int i = 0; i < this.getIrisStructure().length; i++) {
             for (int j = 0; j < this.getIrisStructure()[i].length; j++) {
                 ret.append(Integer.toString(this.getIrisStructure()[i][j]) + ",");
             }
         }
-        ret.append("', " + this.getIsLocked() + ", ");
+        ret.append("', " + this.getIsLocked() + ", '");
         this.getPermissionList().forEach((e) -> {
             ret.append(e.toString() + ',');
         });
